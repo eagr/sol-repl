ee48b292397ff67a74c339c7ed64ef0957d2a297
eagr <eagr@tutanota.com>
2022-04-13 08:50:33 +0800

    rearrange

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs')
 const { ethers } = require('ethers')
 const ganache = require('ganache')
-const yargs = require('yargs')
 const { compile } = require('./compiler')
 const { prompt, help, toPrintable } = require('./repl')
 const pkg = require('../package.json')
+
+const version = pkg.dependencies.solc
+yargs
+    .version(version)
+    .wrap(yargs.terminalWidth())
+    .parse()
 
 ethers.utils.Logger.setLogLevel('OFF')
 const { providers, ContractFactory } = ethers
@@ -14,23 +20,11 @@ const provider = new providers.Web3Provider(ganache.provider({
 }))
 const signer = provider.getSigner()
 
-const version = pkg.dependencies.solc
-yargs
-    .version(version)
-    .wrap(yargs.terminalWidth())
-    .parse()
-
-const { stdin, stdout } = process
-console.log(`Welcome to Solidity v${version}!`)
-console.log('Type ".help" for more information.')
-prompt()
-
 const session = []
-stdin.setEncoding('utf8')
-stdin.resume()
-stdin.on('data', async (inp) => {
+
+async function exec (inp) {
     inp = inp.trim()
-    if (inp === '') return prompt()
+    if (inp === '') return
 
     if (/^\.[A-Za-z]+$/.test(inp)) {
         inp = inp.toLowerCase()
@@ -61,5 +55,16 @@ stdin.on('data', async (inp) => {
             console.log(toPrintable(rawRes))
         }
     }
+}
+
+const { stdin, stdout } = process
+stdin.setEncoding('utf8')
+stdin.resume()
+stdin.on('data', async (inp) => {
+    await exec(inp)
     prompt()
 })
+
+console.log(`Welcome to Solidity v${version}!`)
+console.log('Type ".help" for more information.')
+prompt()
